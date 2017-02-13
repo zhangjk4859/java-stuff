61356359e06af14241791150ab54c3766678a636
zhangjk4859 <573581325@qq.com>
2017-02-13 22:14:20 +0800

    no message

---
+++ staticTest/src/Main.java
@@ -0,0 +1,24 @@
+public class Main {
+
+    public void test(Object o) {
+        System.out.println("Object");
+    }
+    public void test(String s) {
+        System.out.println("String");
+    }
+
+    static String s0, s1;
+    public static void main(String[] args) {
+//        System.out.println("Hello World!");
+
+//        Main that = new Main();
+//        that.test(null);
+//        String a = "abc";
+//        String b = "ab" + "c";
+//        System.out.println(a == b);
+        s0 = s0 + s1;
+        System.out.println(s0);
+
+
+    }
+}
